d5be5f5e9093d67d46b646c0007d1fd9b8ffc2be
Kusal rathna <apple@Kusals-MacBook-Pro.local>
2020-10-01 02:22:33 +0530

    player development done

---
+++ Kathapotha/src/app/player/player.page.ts
@@ -0,0 +1,118 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonRange } from '@ionic/angular';
+import { Howl, Howler } from 'howler';
+
+export interface Track {
+  name: string;
+  path: string;
+}
+
+@Component({
+  selector: 'app-player',
+  templateUrl: './player.page.html',
+  styleUrls: ['./player.page.scss'],
+})
+export class PlayerPage implements OnInit {
+
+  playlist: Track[] = [
+
+    {
+      name: 'Andare - අන්දරේ',
+      path: "https://firebasestorage.googleapis.com/v0/b/lankahub-24c28.appspot.com/o/Video%2Faudio1.mp3?alt=media&token=8a861b26-adc0-4d39-babf-f5114e512656",
+    },
+    // {
+    //   name: 'Gajaman Nona - ගජමන් නෝනා',
+    //   path: "assets/audio/audio2.mp3",
+    // },
+
+  ];
+
+  activeTrack: Track = null;
+  player: Howl = null;
+  isplaying = false;
+  progress = 0;
+  @ViewChild('range', { static: false }) range: IonRange;
+
+  constructor() { }
+
+  ngOnInit() {
+
+
+ 
+  }
+
+
+  start(track: Track) {
+
+    if (this.player) {
+      this.player.stop();
+    }
+
+    this.player = new Howl({
+      src: [track.path],
+      html5: true,
+
+      onplay: () => {
+
+        this.isplaying = true;
+        this.activeTrack = track;
+        this.updateprgress();
+      },
+
+      onend: () => {
+
+      }
+    });
+    this.player.play();
+  }
+
+
+  togglePlayer(pause) {
+
+    this.isplaying = !pause;
+
+    if (pause) {
+      this.player.pause();
+    } else {
+      this.player.play();
+    }
+
+  }
+
+  next() {
+
+
+
+  }
+
+  prev() {
+
+
+  }
+
+  seek() {
+    let newValue = +this.range.value;
+    let duration = this.player.duration();
+    this.player.seek(duration * (newValue / 100));
+
+  }
+
+
+  updateprgress() {
+
+    let seek = this.player.seek();
+    this.progress = (seek / this.player.duration()) * 100 || 0
+    setTimeout(() => {
+      this.updateprgress();
+    }, 1000)
+
+  }
+
+
+  stopplayer(){
+    this.player.stop();
+  }
+
+}
+
+
