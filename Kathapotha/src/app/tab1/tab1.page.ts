be216b85eb7ea4d2b2bff63be333d22242862494
Kusal rathna <apple@Kusals-MacBook-Pro.local>
2020-09-30 23:31:45 +0530

    homepage development done

--- Kathapotha/src/app/tab1/tab1.page.ts
+++ Kathapotha/src/app/tab1/tab1.page.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+
+
+  slideOpts = {
+    autoplay:true,
+    initialSlide: 0,
+    direction: 'horizontal',
+    speed: 300,
+    spaceBetween: 1.3,
+    slidesPerView: 2.2,
+    // freeMode: true,
+    loop: true,
+  };
 
   constructor() {}
 
